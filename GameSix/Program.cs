adf74dae90211dfcc1658c64dd30f8a276c79ed5
Paulo Silva <a22206205@alunos.ulht.pt>
2023-04-21 10:51:27 +0100

    Adicionado construtor

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,10 @@ namespace GameSix
     {
         public class Foe
         {
-            
+            private string name;
+            private float health;
+            private float shield;
+
         }
     }
 
