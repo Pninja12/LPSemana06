986088c2f60d9b2bf51d5c202a17026b7e5a48f4
Paulo Silva <a22206205@alunos.ulht.pt>
2023-04-21 11:51:29 +0100

    Exercício 4 concluído, tive dificulades porque estava a atribuir a uma variável local em vez da variável da classe

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -12,7 +12,7 @@ namespace GameSix
 
             public Foe (string name)
             {
-                this.name = name ; 
+                this.name = name; 
                 health = 100 ;
                 shield = 0 ;
             }
@@ -34,6 +34,46 @@ namespace GameSix
                 }
             }
 
+            public float GetHealth()
+            {
+                return health;
+            }
+
+            public float GetShield()
+            {
+                return shield;
+            }
+
+            public void SetName(string _name)
+            {   
+                string newname = "";
+                string space = " ";
+                char funcspace = char.Parse(space); //como o "name[0]" é um char, tenho de fazer Parse desta variável para char
+                if(_name[0] == funcspace)            //podia apenas criar uma varável char mas não estava a aceitar o espaço então tive que forçar
+                {
+                    for(int i = 1; i <= (_name.Length - 2); i++)
+                    {
+                        newname += _name[i];
+                    }
+                }
+                if(_name[_name.Length - 1] != funcspace)
+                    {
+                        newname += _name[_name.Length -1];
+                    }
+
+                name = newname;
+            }
+            private static void Main(string[] args)
+            {
+                // C r e a t e a new Foe
+            Foe loki = new Foe ("Loki");
+            // Changed my mind . Let ’ s u s e t h e f u l l name .
+            loki.SetName(" Grindalokki ");
+            // Show name and h e a l t h
+            Console.WriteLine($"Name is {loki.GetName()} and HP is {loki.GetHealth()}");
+            }
+
+            
 
         }
     }
